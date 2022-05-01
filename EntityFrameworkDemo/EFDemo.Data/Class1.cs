f5b4c6df013a1a983b03548a58929f1503675f76
Nonso <justcoolk@yahoo.com>
2022-05-01 10:22:19 +0100

    Setup Projects

---
+++ EntityFrameworkDemo/EFDemo.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EFDemo.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ EntityFrameworkDemo/EFDemo.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EFDemo.Domain
+{
+    public class Class1
+    {
+    }
+}
