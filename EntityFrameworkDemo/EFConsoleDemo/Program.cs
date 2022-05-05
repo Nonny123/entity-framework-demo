98d21076ed0c7e12dc70d3949312863ff2514305
Nonso <justcoolk@yahoo.com>
2022-05-05 07:34:49 +0100

    Filter records

--- EntityFrameworkDemo/EFConsoleDemo/Program.cs
+++ EntityFrameworkDemo/EFConsoleDemo/Program.cs
@@ -1,7 +1,9 @@
 ﻿using EFDemo.Data;
 using EFDemo.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EFConsoleDemo
@@ -12,24 +14,59 @@ namespace EFConsoleDemo
 
         static async Task Main(string[] args)
         {
-            //var league = new League { Name = "Bundesliga" };
-            //var team = new Team { Name = "Bayern Munich", League = league };
-            //await context.AddAsync(team);
-            //await context.SaveChangesAsync();
 
+            //await AddNewLeague();
+            //await AddNewTeamsWithLeague();
 
-            var league = new League { Name = "Serie A" };
-            await context.Leagues.AddAsync(league);
-            await context.SaveChangesAsync();
-
+            //await SimpleSelectQuery()
             
-            await AddTeamsWithLeague(league);
-            await context.SaveChangesAsync();
+            await QueryFilters();
 
             Console.WriteLine("press any key");
             Console.ReadKey();
         }
 
+
+        static async Task QueryFilters()
+        {
+            //Console.WriteLine($"Enter League Name: ");
+            Console.WriteLine($"Enter League Name (Or Part Of): ");
+            var leagueName = Console.ReadLine();
+            var exactMatches = await context.Leagues.Where(q => q.Name.Equals(leagueName)).ToListAsync();
+            foreach (var league in exactMatches)
+            {
+                Console.WriteLine($"{league.Id} . {league.Name}");
+            }
+
+            //var partialMatches = await context.Leagues.Where(q => q.Name.Contains(leagueName)).ToListAsync();
+            var partialMatches = await context.Leagues.Where(q => EF.Functions.Like(q.Name,$"%{leagueName}%")).ToListAsync();
+            foreach (var league in partialMatches)
+            {
+                Console.WriteLine($"{league.Id} . {league.Name}");
+            }
+        }
+        
+        static async Task SimpleSelectQuery()
+        {
+            var leagues = await context.Leagues.ToListAsync(); // ToList executes the query, converts to list of objects, then the database conection closes
+            foreach (var league in leagues)
+            {
+                Console.WriteLine($"{league.Id} . {league.Name}");
+            }
+        }
+        
+        static async Task AddNewLeague()
+        {
+            //add new league object
+            var league = new League { Name = "Serie A" };
+            await context.Leagues.AddAsync(league);
+            await context.SaveChangesAsync();
+
+            //add new teams related to the new league object
+            await AddTeamsWithLeague(league);
+            await context.SaveChangesAsync();
+        }
+        
         static async Task AddTeamsWithLeague(League league)
         {
             var teams = new List<Team>
@@ -51,7 +88,16 @@ namespace EFConsoleDemo
                 }
             };
 
+            //add multiple objects to database at once
             await context.AddRangeAsync(teams);
+        }
+        
+        static async Task AddNewTeamsWithLeague()
+        {
+            var league = new League { Name = "Bundesliga" };
+            var team = new Team { Name = "Bayern Munich", League = league };
+            await context.AddAsync(team);
+            await context.SaveChangesAsync();
         }
     }
 }
